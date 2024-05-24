15e3ca727f3b0844fed15492368d9fd86c8dfefa
James-cloudcannon <james@cloudcannon.com>
2024-05-25 08:34:32 +1200

    feat: updates blog list summary

--- src/components/blog/summary.jsx
+++ src/components/blog/summary.jsx
@@ -1,44 +1,75 @@
+import './_summary.scss';
+
 export default function PostSummary({ post }) {
   const {
-    data: { thumb_image_path, thumb_image_alt, title, author, date, tags },
+    data: { thumb_image_path, thumb_image_alt, image, image_alt, title, excerpt, author, date, tags },
     slug,
   } = post;
-
   const dateObj = new Date(date);
+  const formattedDate = dateObj.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
+
   return (
     <>
-      <div className="">
-        <article className="block">
-          <a href={`/blog/${slug}`}>
-            <img
-              className="rounded-lg aspect-video"
-              src={thumb_image_path}
-              alt={thumb_image_alt}
-              loading="lazy"
-            />
-          </a>
-          <div className="blog-post-tag">
-            {tags.slice(0, 2).map((tag, i) => (
-              <a href={`/tags/${tag.toLowerCase()}`} key={i}>
-                {tag[0].toUpperCase() + tag.slice(1)}
-              </a>
-            ))}
+      {post.data.featured ? (
+        <article className="blog-card--featured">
+          <div className="blog-card--featured__wrapper">
+            <a href={`/blog/${slug}`} className="blog-card--featured__wrapper__headline">
+              <h2>{title}</h2>
+            </a>
+            <p className="blog-card--featured__wrapper__date">{formattedDate}</p>
+            <p className="blog-card--featured__wrapper__excerpt">{excerpt}</p>
+            <a href={`/blog/${slug}`} className="blog-card--featured__wrapper__button">Continue reading »</a>
+            <ul className="blog-card--featured__wrapper__author">
+              {author.map((authorName, i) => (
+                <li key={i} className="blog-card--featured__wrapper__author__list">
+                  <a href={`/author/${authorName.name.toLowerCase().replace(' ', '-')}`} className="blog-card--featured__wrapper__author__list__avatar">
+                    <img src={authorName.avatar} alt={authorName.name} width="62" height="62" />
+                  </a>
+                  <div className="blog-card--featured__wrapper__author__list__name">
+                    <a href={`/author/${authorName.name.toLowerCase().replace(' ', '-')}`} className="fw5 f4 no-underline black">{authorName.name}</a>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
-          <a
-            className="flex flex-col justify-start mt-2 max-w-[500px]"
-            href={`/blog/${slug}`}>
-            <p className="text-2xl font-semibold">{title}</p>
-            <p className="font-light">{author}</p>
-            <p className="font-light">
-              {dateObj.toLocaleDateString(undefined, {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-              })}
-            </p>
-          </a>
+          <div className="blog-card--featured__image">
+            <img className="dn di-l" src={image} alt={image_alt} />
+          </div>
         </article>
-      </div>
+      ) : (
+        <article className="blog-card">
+          <div className="blog-card__wrapper">
+            <a href={`/blog/${slug}`} className="blog-card__wrapper__heading">
+              <h2>{title}</h2>
+            </a>
+            <p className="blog-card__wrapper__date">{formattedDate}</p>
+            <div className="blog-card__wrapper__tags">
+              {tags.map((tag, i) => (
+                <a href={`/tags/${tag.toLowerCase()}`} key={i}>
+                  {tag[0].toUpperCase() + tag.slice(1)}
+                </a>
+              ))}
+            </div>
+            <p className="blog-card__wrapper__excerpt">{excerpt}</p>
+            <ul className="blog-card__wrapper__author">
+              {author.map((authorName, i) => (
+                <li key={i} className="blog-card__wrapper__author__list">
+                  <a href={`/author/${authorName.name.toLowerCase().replace(' ', '-')}`} className="blog-card__wrapper__author__list__avatar">
+                    <img className="author-profile-image br-100 mr2" alt={authorName.name} width="62" height="62" src={authorName.avatar} />
+                  </a>
+                  <div className="blog-card__wrapper__author__list__name">
+                    <a href={`/author/${authorName.name.toLowerCase().replace(' ', '-')}`} className="fw4 f3 no-underline black">{authorName.name}</a>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </article>
+      )}
     </>
   );
 }
