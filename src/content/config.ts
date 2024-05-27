87736034b6b449f53b1ecccc7bc98079e8d1e7a0
James-cloudcannon <james@cloudcannon.com>
2024-05-27 14:13:47 +1200

    fix: figure

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,15 +16,15 @@ const blogCollection = defineCollection({
   schema: z.object({
     date: z.string().or(z.date()),
     title: z.string(),
-    excerpt: z.string(),
+    excerpt: z.string().optional(),
     tags: z.array(z.string()),
     author: z.array(
       z.object({
-        name: z.string(),
-        avatar: z.string(),
+        name: z.string().optional(),
+        avatar: z.string().optional(),
         social: z.string().optional(),
       })
-    ),
+    ).optional(),
     thumb_image_path: z.string().optional(),
     thumb_image_alt: z.string().optional(),
     image: z.string().optional(),
